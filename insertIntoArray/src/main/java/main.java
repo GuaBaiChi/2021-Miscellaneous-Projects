c954c3ca5c882816c5c86ae390f62654307ad6e4
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2021-11-28 22:50:26 -0500

    2021.11.28
    2021.11.28

--- insertIntoArray/src/main/java/main.java
+++ insertIntoArray/src/main/java/main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class main {
@@ -43,4 +44,34 @@ public class main {
         }
         // Should print: 5, 1, 10, 2, 3, 200
     }
+
+   public static void insertIntoArray(ArrayList<Integer> array, int data, int position) {
+        //You should preserve the order of the array list after inserting.
+        //Example, if the array is [0,2,3] and you call
+        //insertIntoArray(array, 10, 1) then the array should now be [0,10,2,3].
+        if (position < array.size()){
+            array.add(position,data);
+        }
+        //If the position given is larger than the array.
+        //Add the data to the end of the array.
+        else {
+            array.add(array.size(), data);
+        }
+        //another way you can achieve that, but I will comment that, and you can check it
+        //array.add(Math.min(position, array.size()), data);
+
+        System.out.println(array);
+
+    }
 }
+
+//Create a function called insertIntoArray with the following prototype:
+//The first argument ArrayList<Integer> array is an array of integers.
+//The second argument int data is an integer to be inserted into the array list.
+//The third argument int position is the index within the 
+//array that data should be added to.
+//You should preserve the order of the array list after inserting. 
+//Example, if the array is [0,2,3] and you call 
+//insertIntoArray(array, 10, 1) then the array should now be [0,10,2,3].
+//If the position given is larger than the array. 
+//Add the data to the end of the array. 
